39df400587b390a2f951aaa668651b907f332c55
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-02-03 19:54:23 +0000

    Tested if works with catagory pages

--- WebScrapper/WebScrapper/Program.cs
+++ WebScrapper/WebScrapper/Program.cs
@@ -18,15 +18,18 @@ namespace WebScrapper
         static void Main(string[] args)
         {
             string termToSearchFor = "movie";
-            string url = "https://www.amazon.co.uk/s?k=" + termToSearchFor + "&ref=nb_sb_noss_1";
+            //string url = "https://www.amazon.co.uk/s?k=" + termToSearchFor + "&ref=nb_sb_noss_1";
+
+            //works with catagory page
+            string url = "https://www.amazon.co.uk/s?i=electronics&bbn=560798&rh=n%3A560798%2Cp_6%3AA3P5ROKL5A1OLE&dc&qid=1612380873&rnid=419151031&ref=sr_pg_1";
 
             string searchTerm = "";
 
             //get details from each product on search page
-            ScrapePageDetails detailPageResults = new ScrapePageDetails();
-            var searchLinks = detailPageResults.getSearchPageLinks("https://www.amazon.co.uk/s?k=tv&ref=nb_sb_noss_1");
-            var lstpageDetails = detailPageResults.GetPageDetails(searchLinks, searchTerm);
-            detailPageResults.exportToCsv(lstpageDetails, searchTerm);
+            //ScrapePageDetails detailPageResults = new ScrapePageDetails();
+            //var searchLinks = detailPageResults.getSearchPageLinks("https://www.amazon.co.uk/s?k=tv&ref=nb_sb_noss_1");
+            //var lstpageDetails = detailPageResults.GetPageDetails(searchLinks, searchTerm);
+            //detailPageResults.exportToCsv(lstpageDetails, searchTerm);
 
             //get product info from search page
             ScrapeSearchPage searchResults = new ScrapeSearchPage();
